23d198dbc5726c77a4819ed22dc19443791f10f6
POMBO21052004 <albanpombombe@gmail.com>
2026-01-14 12:53:45 +0100

    update

--- src/components/NotificationDropdown.jsx
+++ src/components/NotificationDropdown.jsx
@@ -19,7 +19,7 @@ const NotificationDropdown = () => {
 
     // Get token for WebSocket auth (if your backend supports it via query param or header)
     const token = localStorage.getItem('token');
-    const WS_URL = token ? `ws://https://digiplus-grh-production.up.railway.app/ws/notifications/?token=${token}` : null;
+    const WS_URL = token ? `wss://digiplus-grh-production.up.railway.app/ws/notifications/?token=${token}` : null;
 
     const { lastJsonMessage, readyState } = useWebSocket(WS_URL, {
         shouldReconnect: (closeEvent) => true,
