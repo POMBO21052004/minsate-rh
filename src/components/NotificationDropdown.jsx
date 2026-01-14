2a17bd36248ac692f075f5ccc2108b88beac28e7
POMBO21052004 <albanpombombe@gmail.com>
2026-01-14 12:46:43 +0100

    update

--- src/components/NotificationDropdown.jsx
+++ src/components/NotificationDropdown.jsx
@@ -19,7 +19,10 @@ const NotificationDropdown = () => {
 
     // Get token for WebSocket auth (if your backend supports it via query param or header)
     const token = localStorage.getItem('token');
-    const WS_URL = token ? `ws://127.0.0.1:8000/ws/notifications/?token=${token}` : null;
+    // const WS_URL = token ? `ws://127.0.0.1:8000/ws/notifications/?token=${token}` : null;
+
+    // En production sur Railway
+    const WS_URL = token ? `wss://digiplus-grh-production.up.railway.app/ws/notifications/?token=${token}` : null;
 
     const { lastJsonMessage, readyState } = useWebSocket(WS_URL, {
         shouldReconnect: (closeEvent) => true,
